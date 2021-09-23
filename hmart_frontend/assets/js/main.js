25c27c4b0011d82c6c5115491fc9d54c123d155b
gabil-gurbanov-gadir <gabilgg@code.edu.az>
2021-09-23 12:58:39 +0400

    All canvases created

--- hmart_frontend/assets/js/main.js
+++ hmart_frontend/assets/js/main.js
@@ -22,4 +22,56 @@ $(document).ready(function () {
       $("#header-nav-menu").removeClass("fixed animate__fadeInDown");
     }
   });
+
+  $(document).on("click", "#wishlist-button", function (e) {
+    e.preventDefault();
+    $("body").addClass("canvas-opening");
+    $("#shadow-layout").removeClass("d-none");
+    $("#wishlist .list-wrapper").addClass("canvas-opening");
+  });
+
+  $(document).on(
+    "click",
+    "#wishlist .canvas-close, #shadow-layout",
+    function (e) {
+      e.preventDefault();
+      $("body").removeClass("canvas-opening");
+      $("#shadow-layout").addClass("d-none");
+      $("#wishlist .list-wrapper").removeClass("canvas-opening");
+    }
+  );
+
+  $(document).on("click", "#cartlist-button", function (e) {
+    e.preventDefault();
+    $("body").addClass("canvas-opening");
+    $("#shadow-layout").removeClass("d-none");
+    $("#cart .list-wrapper").addClass("canvas-opening");
+  });
+
+  $(document).on("click", "#cart .canvas-close, #shadow-layout", function (e) {
+    e.preventDefault();
+    $("body").removeClass("canvas-opening");
+    $("#shadow-layout").addClass("d-none");
+    $("#cart .list-wrapper").removeClass("canvas-opening");
+  });
+
+  $(document).on("click", "#menu-button", function (e) {
+    e.preventDefault();
+    $("body").addClass("canvas-opening");
+    $("#shadow-layout").removeClass("d-none");
+    $("#mobile-menu .menu").addClass("canvas-opening");
+    $("#mobile-menu .canvas-close").addClass("move");
+  });
+
+  $(document).on(
+    "click",
+    "#mobile-menu .canvas-close, #shadow-layout",
+    function (e) {
+      e.preventDefault();
+      $("body").removeClass("canvas-opening");
+      $("#shadow-layout").addClass("d-none");
+      $("#mobile-menu .menu").removeClass("canvas-opening");
+      $("#mobile-menu .canvas-close").removeClass("move");
+    }
+  );
 });
